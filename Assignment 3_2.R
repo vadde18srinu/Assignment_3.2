615a6021144b6b87fa50d058601b33e75eb76daf
vadde18srinu <40682358+vadde18srinu@users.noreply.github.com>
2018-08-06 18:39:17 +0530

    Add files via upload

--- Assignment 3_2.R
+++ Assignment 3_2.R
@@ -1,13 +1,22 @@
 1. Define matrix mymat by replicating the sequence 1:5 for 4 times 
    and transforming into a matrix, sum over rows and columns.
 
-   
+
+#Type-1      
 m=matrix(1:5, 5,4)
 m
+colSums(m,na.rm = FALSE,dims = 1)
+rowSums(m,na.rm = FALSE,dims = 1)
 sum(m,na.rm=TRUE)
 
+
+#Type-2
 mymat=matrix(rep(seq(5), 4), ncol = 5) 
 mymat
-
+colSums(mymat,na.rm = FALSE,dims = 1)
+rowSums(mymat,na.rm = FALSE,dims = 1)
 sum(mymat,na.rm = TRUE)
+
+
+
 
