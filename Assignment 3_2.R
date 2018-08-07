01cb10716143eb396dab3c0b3ef582fbce08466c
vadde18srinu <40682358+vadde18srinu@users.noreply.github.com>
2018-08-07 12:05:42 +0530

    Add files via upload

--- Assignment 3_2.R
+++ Assignment 3_2.R
@@ -3,20 +3,18 @@
 
 
 #Type-1      
+mymat=matrix(rep(seq(5), 4), ncol = 5) 
+mymat
+colSums(mymat,na.rm = FALSE,dims = 1)
+rowSums(mymat,na.rm = FALSE,dims = 1)
+sum(mymat,na.rm = TRUE)   
+   
+
+#Type-2
 m=matrix(1:5, 5,4)
 m
 colSums(m,na.rm = FALSE,dims = 1)
 rowSums(m,na.rm = FALSE,dims = 1)
 sum(m,na.rm=TRUE)
-
-
-#Type-2
-mymat=matrix(rep(seq(5), 4), ncol = 5) 
-mymat
-colSums(mymat,na.rm = FALSE,dims = 1)
-rowSums(mymat,na.rm = FALSE,dims = 1)
-sum(mymat,na.rm = TRUE)
-
-
 
 
